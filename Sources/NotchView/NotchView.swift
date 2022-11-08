3114e3a45c0c48e9390edcd2a0850b2e6803666d
Yorwos Pallikaropoulos <yorwos@icloud.com>
2022-11-08 23:18:14 +0200

    initial commit

--- Sources/NotchView/NotchView.swift
+++ Sources/NotchView/NotchView.swift
@@ -1,6 +1,88 @@
-public struct NotchView {
-    public private(set) var text = "Hello, World!"
+//
+//  NotchView.swift
+//  Notch View Project
+//
+//  Created by George Pallikaropoulos on 6/11/22.
+//
 
-    public init() {
+import UIKit
+
+class NotchView:UIView {
+    
+
+
+// MARK: - properties
+    public var name:Name = .iPhone14Plus{
+        didSet{
+            setupView()
+        }
     }
+    
+    public var color:UIColor = UIColor.black{
+        didSet{
+            self.backgroundColor = color
+        }
+    }
+
+    public var radius:CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = radius
+        }
+    }
+    
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+
+        super.init(frame: frame)
+        print("super was called with frame:\(frame)")
+      
+        
+        setupView()
+
+        
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    
+
+    
+    private func setupView(){
+    //private func setupView(name:Name, nativeScale:CGFloat){
+        guard let name = Name(rawValue: UIDevice.current.name) else { return }
+        let nativeScale = UIScreen.main.nativeScale
+        guard let  metric = Metrics(name: name, nativeScale: nativeScale) else {
+            print("cannot find metric for \(name.rawValue)")
+            
+            return
+            
+        }
+
+        self.backgroundColor = color
+        self.layer.cornerRadius = metric.bottomRadius
+        self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner] // which corners will be rounded
+        self.frame = CGRect(origin: CGPoint(), size: metric.size)
+        
+        print("frame is:\(self.frame)")
+        
+        
+    }
+    
+    
+    
+
+    
+    
+
 }
+
+
+
--- Tests/NotchViewTests/NotchViewTests.swift
+++ Tests/NotchViewTests/NotchViewTests.swift
@@ -6,6 +6,6 @@ final class NotchViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NotchView().text, "Hello, World!")
+//        XCTAssertEqual(NotchView().text, "Hello, World!")
     }
 }
